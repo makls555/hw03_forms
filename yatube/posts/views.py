607e1c73b1ea56aff0d987604bd8f15b839e2bf3
Никита Бобков <n.bobkov99@bk.ru>
2022-12-20 00:19:48 +0300

    ГотовоРевью3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,95 +1,68 @@
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Group, Post, User
 from .forms import PostForm
-
-
-def page_look(post_list, request):
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return page_obj
+from .utils import get_pagin
 
 
 def index(request):
-    post_list = Post.objects.select_related('author')
-    page_obj = page_look(post_list, request)
-    context = {
-        'page_obj': page_obj,
-    }
+    context = get_pagin(Post.objects.all(), request)
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all()
-    page_obj = page_look(post_list, request)
-    title = f'Записи сообщества {group.title}'
-    description = group.description
     context = {
         'group': group,
-        'page_obj': page_obj,
-        'title': title,
-        'description': description,
+        'post_list': post_list
     }
+    context.update(get_pagin(group.posts.all(), request))
     return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    author_posts = author.posts.all()
-    posts_count = author.posts.count()
-    page_obj = page_look(author_posts, request)
     context = {
         'author': author,
-        'posts': author_posts,
-        'posts_count': posts_count,
-        'page_obj': page_obj,
     }
+    context.update(get_pagin(author.posts.all(), request))
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    posts_count = post.author.posts.count()
-    title = post.text[:30]
+    post = get_object_or_404(Post, id=post_id)
     context = {
-        'posts': post,
-        'posts_count': posts_count,
-        'title': title,
+        'post': post,
     }
     return render(request, 'posts/post_detail.html', context)
 
 
 @login_required
 def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', post.author.username)
-    else:
-        form = PostForm()
-    return render(request, 'posts/post_create.html', {'form': form})
+    form = PostForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author)
+    return render(request, 'posts/post_create.html', context)
 
 
 @login_required
 def post_edit(request, post_id):
-    """Функция для редактирования поста"""
     post = get_object_or_404(Post, pk=post_id)
-    form = PostForm(request.POST, instance=post)
     if post.author != request.user:
-        return redirect('posts:post_detail', post_id)
-    if form.is_valid():
+        return redirect('posts:post_detail', post.pk)
+    form = PostForm(request.POST or None, instance=post)
+    if request.method == 'POST' and form.is_valid():
         form.save()
-        return redirect('posts:post_detail', post_id)
+        return redirect('posts:post_detail', post.pk)
     context = {
-        'form': PostForm(instance=post),
-        'post': post,
-        'is_edit': True
+        'form': form,
     }
     return render(request, 'posts/post_create.html', context)
