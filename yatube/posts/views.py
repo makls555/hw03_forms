b4e5c776f20d65555aca9ec610ca01545926c95d
Никита Бобков <n.bobkov99@bk.ru>
2022-12-19 19:01:42 +0300

    ГотовоРевью3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.decorators import login_required
-
 from django.core.paginator import Paginator
-
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Group, Post, User
-
 from .forms import PostForm
 
 
